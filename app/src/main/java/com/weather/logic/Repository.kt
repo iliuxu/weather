d44fee457893bb058d557b29f4284593d91f39c0
iliuxu <imliuxu@gamil.com>
2020-04-13 19:59:13 +0800

    显示天气信息

--- app/src/main/java/com/weather/logic/Repository.kt
+++ app/src/main/java/com/weather/logic/Repository.kt
@@ -3,11 +3,14 @@ package com.weather.logic
 import android.util.Log
 import androidx.lifecycle.liveData
 import com.weather.logic.model.Place
-import com.weather.logic.model.PlaceResponse
+import com.weather.logic.model.Weather
 import com.weather.logic.network.WeatherNetwork
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import java.lang.Exception
 import java.lang.RuntimeException
+import kotlin.coroutines.CoroutineContext
 
 object Repository {
     fun searchPlaces(query: String) = liveData(Dispatchers.IO) {
@@ -25,4 +28,42 @@ object Repository {
         }
         emit(result as Result<*>)
     }
+    fun refreshWeather(lng: String, lat: String, placeName: String) = liveData(Dispatchers.IO) {
+        val  result = try {
+            coroutineScope {
+                val deferredRealtime = async {
+                    WeatherNetwork.getRealtimeWeather(lng, lat)
+                }
+                val deferredDaily = async {
+                    WeatherNetwork.getDailyWeather(lng, lat)
+                }
+                val realtimeResponse = deferredRealtime.await()
+                val dailyResponse = deferredDaily.await()
+                if (realtimeResponse.status == "ok" && dailyResponse.status == "ok") {
+                    val weather = Weather(realtimeResponse.result.realtime,
+                        dailyResponse.result.daily)
+                    Result.success(weather)
+                } else {
+                    Result.failure(
+                        RuntimeException(
+                            "realtime response status is ${realtimeResponse.status}" +
+                                    "daily response status is ${dailyResponse.status}"
+                        )
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            Result.failure<Weather>(e)
+        }
+        emit(result as Result<Weather>)
+    }
+    private fun <T> fire(context: CoroutineContext, block: suspend() -> Result<T>)  =
+        liveData<Result<T>>(context) {
+            val result = try {
+                block()
+            } catch (e: Exception) {
+                Result.failure<T>(e)
+            }
+            emit(result)
+        }
 }
